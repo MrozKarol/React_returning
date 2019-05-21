5894b0b2f24aa85db075ddcbb1a24e4b767a0d83
MrozKarol <polityk357@gmail.com>
2019-05-21 20:42:49 +0200

    wykorzystanie metodt render do wygnerowania strony

--- App.js
+++ App.js
@@ -1,4 +1,21 @@
 // console.log(React);
 // console.log(React);
 
+const header = <h1 className="title">Lorem ipsum dolor </h1>
+const main = (
+  <div>
+    <h1>Pierwszy artykuł</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae consectetur sunt voluptatibus veniam fugit quo nihil, ad provident eius veritatis maxime numquam. Nesciunt, optio. Dolorum eum iure laboriosam praesentium consectetur!</p>
+  </div>
+)
 
+const footer = (
+  <footer>
+    <p>Stipka</p>
+  </footer>
+)
+
+const app = [header, main, footer]
+
+
+ReactDOM.render(app, document.getElementById('root'))
