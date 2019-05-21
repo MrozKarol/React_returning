88e774413b29424f8b37512abe663ade3dbe3cba
MrozKarol <polityk357@gmail.com>
2019-05-21 20:55:56 +0200

    przekazanie funkcji alert

--- App.js
+++ App.js
@@ -5,9 +5,11 @@ const header = <h1 className="title">Lorem ipsum dolor </h1>
 
 const classBig = "big"
 
+const handleClick = () => alert("kliknął")
+
 const main = (
   <div>
-    <h1 className="red" >Pierwszy artykuł</h1>
+    <h1 onClick={ handleClick}  className="red" >Pierwszy artykuł</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae consectetur sunt voluptatibus veniam fugit quo nihil, ad provident eius veritatis maxime numquam. Nesciunt, optio. Dolorum eum iure laboriosam praesentium consectetur!</p>
   </div>
 )
