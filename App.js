fb6a1226abd4371425a53b4c777d7915f48dcd72
MrozKarol <polityk357@gmail.com>
2019-05-21 20:50:52 +0200

    ostylowanie za pomoca css elementu h1, wstrzykniecie klasy do elementu p footer

--- App.js
+++ App.js
@@ -2,16 +2,20 @@
 // console.log(React);
 
 const header = <h1 className="title">Lorem ipsum dolor </h1>
+
+const classBig = "big"
+
 const main = (
   <div>
-    <h1>Pierwszy artykuł</h1>
+    <h1 className="red" >Pierwszy artykuł</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae consectetur sunt voluptatibus veniam fugit quo nihil, ad provident eius veritatis maxime numquam. Nesciunt, optio. Dolorum eum iure laboriosam praesentium consectetur!</p>
   </div>
 )
 
+
 const footer = (
   <footer>
-    <p>Stipka</p>
+    <p className={classBig} >Stipka</p>
   </footer>
 )
 
