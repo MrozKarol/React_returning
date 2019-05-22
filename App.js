6be0764f614b1b67fcd75d846dc2a364e0fc8c25
MrozKarol <polityk357@gmail.com>
2019-05-22 20:02:08 +0200

    stworzenie komponentu funkcyjnego bez stanowego nazwa powinna zaczynać sie z duzej litery

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
-// console.log(React);
-// console.log(React);
+//komponent funkcyjny, bezstanowy
 
-const header = <h1 className="title">Lorem ipsum dolor </h1>
-
-const classBig = "big"
-
-const handleClick = () => alert("kliknął")
-
-const main = (
-  <div>
-    <h1 onClick={ handleClick}  className="red" >Pierwszy artykuł</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae consectetur sunt voluptatibus veniam fugit quo nihil, ad provident eius veritatis maxime numquam. Nesciunt, optio. Dolorum eum iure laboriosam praesentium consectetur!</p>
-  </div>
-)
-
-const textExample ="Lorem Lorem Lorem Lorem Lorem Lorem"
-const footer = (
-  <footer>
-    <p className={classBig} >Stipka {textExample}</p> 
-  </footer>
-)
-
-const app = [header, main, footer]
+const App = () => {
+  return (
+    <h1>Pierwszy komponenet</h1> 
+  )
+}
 
 
-ReactDOM.render(app, document.getElementById('root'))
+
+
+//komponenet klasowy, stanowy
+
+
+ReactDOM.render(<App/>, document.getElementById('root'))
