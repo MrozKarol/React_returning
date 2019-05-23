441f0fd8935439b268a0675d640417cf1b5b7918
MrozKarol <polityk357@gmail.com>
2019-05-23 18:42:03 +0200

    mini app Przycisk po kliknieciu dodawana jest ltera do tekstu

--- App.js
+++ App.js
@@ -1,42 +1,30 @@
-class ShoopingList extends React.Component {
+//Przycisk po kliknieciu dodawana jest ltera do tekstu
+
+class App extends React.Component{
 
   state = {
-    items1: 'pomidory',
-    items2: 'sok',
-    items3: 'kawka'
+    text: ''
   }
 
-
-  render() {
-    return (
-      <div>
-        <h1>Lista zakupow</h1>
-        <ul>
-          <ItemList name = {this.state.items1} example= {2+2} />
-          <ItemList name = {this.state.items2} />
-          <ItemList name = {this.state.items3} />
-     
-        </ul>
-
-      </div>
-    )
+  handleClick = () => {
+    //this.state.text += 'a';
+    //console.log(this.state.text)
+    const letter = 'a'
+    this.setState({
+      text: this.state.text + letter
+    })
   }
-}
-
-// const ItemList = (props) => {
-//   return(
-//     <li>{props.name},- {props.example}</li>
-//   )
-// }
-
-
-class ItemList extends React.Component{
 
   render(){
     return(
-      <li>{this.props.name},- {this.props.example}</li>
+      <>
+      <button onClick={this.handleClick} >Dodaj "A"</button>
+      <h1>{this.state.text}</h1>
+      </>
     )
   }
 }
 
-ReactDOM.render(<ShoopingList />, document.getElementById('root'))
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+
