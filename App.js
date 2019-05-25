71dcc5a08aea40bd36af9cc69aec2767f347cdeb
MrozKarol <polityk357@gmail.com>
2019-05-25 11:45:50 +0200

    WIP pokazywanie wiadomości za pomocą przycisku

---
+++ App.js
@@ -0,0 +1,32 @@
+//Pokazywanie wiadomości za pomocą przycisku
+
+class Message extends React.Component {
+
+   constructor(props){
+       super(props);
+       this.state = {
+           messageIsActive: false
+       }
+       this.handleMessageButton = this.handleMessageButton.bind(this)
+   }
+
+   handleMessageButton() {
+       
+       this.setState({
+           messageIsActive: !this.state.messageIsActive
+       })
+   }
+    render(){
+     console.log(this.state.messageIsActive)
+    const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt iure ea nam officiis, aliquid ipsa earum enim sit natus fugit consequuntur maiores quibusdam vitae iste quo, incidunt deserunt eveniet delectus.'    
+        return(
+            <>
+            <button onClick={this.handleMessageButton} >
+            {this.state.messageIsActive ? 'Ukryj' : 'Pokaż'}
+            </button>
+            <p>{this.state.messageIsActive ? text: !text}</p>
+            </>
+        )
+    }
+}
+ReactDOM.render(<Message />, document.getElementById('root'))
