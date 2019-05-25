8fd5df96125c6a26730395c477b957effdc823b7
MrozKarol <polityk357@gmail.com>
2019-05-25 11:20:18 +0200

    Pokazywanie wiadomości za pomocą przycisku

--- App.js
+++ App.js
@@ -1,36 +1,7 @@
-//wpisywanie textu w input i wyświetlanie w h1
+//Pokazywanie wiadomości za pomocą przycisku
 
 class App extends React.Component {
 
-  state = {
-    value : ""
-  }
+  
 
-  handleInputChange = (e) => {
-
-    
-    console.log(e.target.value);
-    this.setState({value: e.target.value})
-    
-  }
-
-  handleRestClick = () =>{
-    return(
-      this.setState({value: ""})
-    )
-  }
-
-  render() {
-    return (
-      <>
-      <input value={this.state.value} placeholder="wpisz coś..." onChange={this.handleInputChange} type="text" />
-      <button onClick={this.handleRestClick}>Reset</button>
-      <h1 className="title">{this.state.value.toLocaleUpperCase()}</h1>
-      </>
-    )
-
-  }
-
-}
-
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
