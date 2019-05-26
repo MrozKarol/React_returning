0b73831aed34f637ceb2e45efd4df0a1f390336e
MrozKarol <polityk357@gmail.com>
2019-05-26 11:05:57 +0200

    App Counter: liczba klikniec i reset wyniku

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
-//Pokazywanie wiadomości za pomocą przycisku
+//App Counter liczba kliknieć
 
-class Message extends React.Component {
-
-//    constructor(props){
-//        super(props);
-//        this.state = {
-//            messageIsActive: false
-//        }
-//        this.handleMessageButton = this.handleMessageButton.bind(this)
-//    }
-
-     state = {
-        messageIsActive: false
-     }
-
-   handleMessageButton =() => {
-       
-       this.setState({
-           messageIsActive: !this.state.messageIsActive
-       })
-   }
+class Counter extends React.Component {
     render(){
-     console.log(this.state.messageIsActive)
-    const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt iure ea nam officiis, aliquid ipsa earum enim sit natus fugit consequuntur maiores quibusdam vitae iste quo, incidunt deserunt eveniet delectus.'    
         return(
-            <>
-            <button onClick={this.handleMessageButton} >
-            {this.state.messageIsActive ? 'Ukryj' : 'Pokaż'}
-            </button>
-            {/* <p>{this.state.messageIsActive && text}</p> */}
-            {this.state.messageIsActive ? <p>{text}</p> : null}
-            </>
+            
         )
     }
 }
-ReactDOM.render(<Message />, document.getElementById('root'))
+
+
+ReactDOM.render(<Counter />, document.getElementById('root'))
