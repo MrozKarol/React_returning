4edfa941a97a82552e2b13b538bbf2b91470aecf
MrozKarol <polityk357@gmail.com>
2019-06-06 18:11:32 +0200

    Projekt Koszyk

---
+++ Projekty/Projekt Koszyk/Koszyk 1 struktura i działenie aplikacji.js
@@ -0,0 +1,14 @@
+class App extends React.Component {
+    state = {}
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+        
+        
+    
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
