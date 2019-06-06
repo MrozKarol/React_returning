695db8df1e2a9178efc9fbd051dce25cc18d3c9d
MrozKarol <polityk357@gmail.com>
2019-06-06 19:06:09 +0200

    dodanie logiki przecisku dodawania

--- Projekty/Projekt Koszyk/Koszyk 1 struktura i działenie aplikacji.js
+++ Projekty/Projekt Koszyk/Koszyk 1 struktura i działenie aplikacji.js
@@ -4,27 +4,20 @@ class App extends React.Component {
         availableProducts: 6,
         shoppingCart: 0,
     }
-
     handleRemoveFromCard = () => (
         this.setState({shoppingCart: this.state.shoppingCart -1})
     )
-
     handleAddFromCard = () =>(
         this.setState({shoppingCart: this.state.shoppingCart +1})
     )
-
     render(){
         return(
             <div>
-                <button disabled ={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCard} >-</button>
+                <button disabled = {this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCard} >-</button>
                 <span> {this.state.shoppingCart} </span>
-                <button onClick={this.handleAddFromCard} > +</button>
+                <button disabled= {this.state.shoppingCart === this.state.availableProducts ? true : false} onClick={this.handleAddFromCard} > +</button>
             </div>
         )
     }
-        
-        
-    
 }
-
 ReactDOM.render(<App/>, document.getElementById('root'))
