820cd00bd64cb84e004845deaa98de7f5193e8ae
MrozKarol <polityk357@gmail.com>
2019-06-08 14:04:55 +0200

    add Koszyk 2 warunowe dodawanie przyisku.js

---
+++ Projekty/Projekt Koszyk/Koszyk 2warunkowe dodanie prztcsku.js
@@ -0,0 +1,21 @@
+// Watunkowe dodanie przcisku i obsługa stanu magazynu 
+// wszarzenie liczny 0 -CSS
+// destrukteryzacja
+
+class App extends React.Component {
+    
+    
+    state = {
+
+    }
+
+    render (){
+        return (
+            <>
+            
+            </>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
