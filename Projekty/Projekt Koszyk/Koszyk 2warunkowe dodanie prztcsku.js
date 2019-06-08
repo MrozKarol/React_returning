70b6793981ab6491fa0f3432fb9078d1ca346e30
MrozKarol <polityk357@gmail.com>
2019-06-08 14:56:43 +0200

    dodanie warunkowego przycisku kup i obsługa stanu magazynu

--- Projekty/Projekt Koszyk/Koszyk 2warunkowe dodanie prztcsku.js
+++ Projekty/Projekt Koszyk/Koszyk 2warunkowe dodanie prztcsku.js
@@ -18,12 +18,20 @@ class App extends React.Component {
         this.setState({shoppingCard: this.state.shoppingCard +1})
     }
 
+    handleBuy = () => {
+        this.setState({
+            availableProducts: this.state.availableProducts - this.state.shoppingCard,
+            shoppingCard: this.state.shoppingCard = 0
+        })
+    }
+
     render (){
         return (
             <>
             <button disabled={this.state.shoppingCard ? false : true} onClick={this.handleRamoveFromCard} >-1 </button>
             <span> {this.state.shoppingCard} </span>
             <button disabled={this.state.shoppingCard === this.state.availableProducts ? true : false} onClick={this.handleAddToCard}>+1</button>
+            {this.state.shoppingCard > 0 && <button onClick={this.handleBuy}>Kup</button>}
             </>
         )
     }
