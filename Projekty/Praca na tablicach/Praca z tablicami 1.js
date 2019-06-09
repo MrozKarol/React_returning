9f7b4712be2ccbaad682a68d27d88c3f3a98b8d1
MrozKarol <polityk357@gmail.com>
2019-06-09 10:09:12 +0200

    dodanie nowergo pliku Praca z tablicami wyświetlanie elemntow tablicy jako lisy

---
+++ Projekty/Praca na tablicach/Praca z tablicami 1.js
@@ -0,0 +1,11 @@
+class ListItem extends React.Component {
+
+   render(){
+       return(
+           
+           
+       )
+   }
+}
+
+ReactDOM.render(<ListItem /> , document.getElementById('root'))
