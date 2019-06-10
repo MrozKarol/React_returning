3535c7c1ab8194b192c84a7b88cd21689c866565
MrozKarol <polityk357@gmail.com>
2019-06-10 19:24:56 +0200

    wyświeylenie listy za pomoca metody map()

--- Projekty/Praca na tablicach/Praca z tablicami 1.js
+++ Projekty/Praca na tablicach/Praca z tablicami 1.js
@@ -1,9 +1,12 @@
 class ListItem extends React.Component {
-
+    state ={
+        items: ["jabłko", "śliwka", "gruszka"]
+    }
    render(){
        return(
-           
-           
+           <ul>
+              {this.state.items.map((item)=> <li key={item}>{item}</li>)}
+           </ul> 
        )
    }
 }
