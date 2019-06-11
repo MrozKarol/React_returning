02b19362d68f2ba54cc315b20d32fe4c052784c4
MrozKarol <polityk357@gmail.com>
2019-06-11 18:33:19 +0200

    wyswietlenie za pomoca metody filter tylko mezczyzn

--- Projekty/Praca z obiektami/Praca na obiektach.js
+++ Projekty/Praca z obiektami/Praca na obiektach.js
@@ -4,24 +4,46 @@ const data ={
             id: 1,
             age:29,
             name: "Sylweter",
+            sex: "male"
         },
         {
             id: 2,
             age:49,
             name: "Janusz",
+            sex: "male"
         },
         {
             id: 3,
             age:19,
             name: "Tadeusz",
+            sex: "male"
         },
-
+        {
+            id: 4,
+            age:20,
+            name: "Joanna",
+            sex: "female"
+        },
+        {
+            id: 5,
+            age:19,
+            name: "Zuzanna",
+            sex: "female"
+        },
+        {
+            id: 6,
+            age:30,
+            name: "Karol",
+            sex: "male"
+        },
     ]
 }
 const Item = ({user}) => (
-    <div>
-        <p>Użytkownik {user.name}</p>
-        <p>Ma {user.age} lat</p>
+    <div className="userInfo">
+        <h2>{user.name}</h2>
+        <p>Informacje o uzytkowniku</p>
+        <p>Wiek użytkownika <strong>{user.age}</strong></p>
+        <p>Płeć użytkownika <strong>{user.sex}</strong></p>
     </div>
 )
 
@@ -31,7 +53,8 @@ class ListItem extends React.Component {
     // }
    render(){
 
-    const users = this.props.data.users 
+    let users = this.props.data.users
+    users = users.filter((user)=>user.sex === "male")
     const Items = users.map(user => <Item key={user.id} user={user} />)
 
        return(
