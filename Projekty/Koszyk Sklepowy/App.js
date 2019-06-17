10ea2d9a97265bcb1bf21e5de837fcddf2fa2780
MrozKarol <polityk357@gmail.com>
2019-06-17 16:59:18 +0200

    dodanie state

--- Projekty/Koszyk Sklepowy/App.js
+++ Projekty/Koszyk Sklepowy/App.js
@@ -1,5 +1,14 @@
 class App extends React.Component {
-    state = {  }
+    state = { 
+        items:[
+            {id: 1, name:'herbata', active:true},
+            {id: 2, name:'ziemniaki', active:false},
+            {id: 3, name:'kasza', active:false},
+            {id: 4, name:'zupa pomidorowa', active:false},
+            {id: 5, name:'wrzatek', active:false},
+            {id: 6, name:'chleb', active:true},  
+        ],
+     }
     render() { 
         return ( 
             <div>Menu</div>
