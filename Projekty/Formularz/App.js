843486c366aa9121619f32f68f923a8005bbe7a1
MrozKarol <polityk357@gmail.com>
2019-06-24 18:56:08 +0200

    aplkacja formularz dodanie pliku index.html, app.js

---
+++ Projekty/Formularz/App.js
@@ -0,0 +1,10 @@
+class Form extends React.Component {
+
+
+    render(){
+        return(
+            <div>Formularz</div>
+        )
+}
+}
+ReactDOM.render(<Form /> , document.getElementById('root'))
