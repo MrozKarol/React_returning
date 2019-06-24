5e514ba9481292fec1c199310dae7658e8f25544
MrozKarol <polityk357@gmail.com>
2019-06-24 19:10:11 +0200

    dodanie textarea i metody handleTextChange która obsługuje zmiane w textarea

--- Projekty/Formularz/App.js
+++ Projekty/Formularz/App.js
@@ -2,12 +2,19 @@ class Form extends React.Component {
 
     state ={
         city:"Gdańsk",
+        text:"",
 
     }
 
     handleCityChange = (e) =>{
         this.setState({
             city: e.target.value
+        })
+    }
+
+    handleTextChange = (e) =>{
+        this.setState({
+            text: e.target.value
         })
     }
 
@@ -18,6 +25,10 @@ class Form extends React.Component {
                 <label>
                     Podaj misato:
                     <input value={this.state.city} onChange={this.handleCityChange} type="text"/>
+                </label>
+                <br/>
+                <label>Napisz coś o tym mieście
+                    <textarea value={this.state.text} onChange={this.handleTextChange} ></textarea>
                 </label>
             </div>
         )
