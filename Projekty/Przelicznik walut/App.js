a38cf435578e272d924aeeae73522e73f967ff7d
MrozKarol <polityk357@gmail.com>
2019-06-26 16:25:37 +0200

    dodanie plików przelicznik walut index.html i App.js

---
+++ Projekty/Przelicznik walut/App.js
@@ -0,0 +1,10 @@
+class ExchangeCounter extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Kalkulator</div>
+         )
+    }
+}
+ 
+ReactDOM.render(<ExchangeCounter/>, document.getElementById('root'))
