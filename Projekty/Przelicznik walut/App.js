0770bb4bb477c4e04ced31d9a759ab7736d65f78
MrozKarol <polityk357@gmail.com>
2019-06-26 16:56:25 +0200

    wyświetlenie wartości domyślnej za pomocą ternary operatora

--- Projekty/Przelicznik walut/App.js
+++ Projekty/Przelicznik walut/App.js
@@ -1,9 +1,9 @@
 const Dollars = (props) =>(
-    <div>Wartość w dolarach:{(props.cash / props.ratio).toFixed(2)}</div>
+    <div>Wartość w dolarach:{props.cash <= 0 ? "" :(props.cash / props.ratio).toFixed(2)}</div>
 )
 
 const Euros = (props) => (
-    <div>Wartość w euro:{(props.cash / props.ratio).toFixed(2)}</div>
+    <div>Wartość w euro:{props.cash <= 0 ? "" :(props.cash / props.ratio).toFixed(2)}</div>
 )
 
 class ExchangeCounter extends React.Component {
