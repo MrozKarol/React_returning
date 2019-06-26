e2ca84e226551eed92b1ea4eed3c13ac1059c3de
MrozKarol <polityk357@gmail.com>
2019-06-26 17:06:34 +0200

    dest. dodanie componentu Cash

--- Projekty/Przelicznik walut/App.js
+++ Projekty/Przelicznik walut/App.js
@@ -1,35 +1,49 @@
-const Dollars = (props) =>(
-    <div>Wartość w dolarach:{props.cash <= 0 ? "" :(props.cash / props.ratio).toFixed(2)}</div>
-)
+// const Dollars = (props) =>(
+//     <div>Wartość w dolarach:{props.cash <= 0 ? "" :(props.cash / props.ratio).toFixed(2)}</div>
+// )
 
-const Euros = (props) => (
-    <div>Wartość w euro:{props.cash <= 0 ? "" :(props.cash / props.ratio).toFixed(2)}</div>
-)
+// const Euros = (props) => (
+//     <div>Wartość w euro:{props.cash <= 0 ? "" :(props.cash / props.ratio).toFixed(2)}</div>
+// )
+
+const Cash = props => (
+  <div>
+    {props.title}
+    {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}
+  </div>
+);
 
 class ExchangeCounter extends React.Component {
-    state = { 
-        amount: "",
-        ratioDollar: 3.6,
-        ratioEuro: 4.2
-     }
+  state = {
+    amount: "",
+    ratioDollar: 3.6,
+    ratioEuro: 4.2
+  };
 
-    handleChange = (e) =>{
-        this.setState({
-            amount: e.target.value
-        })
-    } 
-    render() { 
-        const {amount, ratioEuro, ratioDollar} = this.state
-        return ( 
-            <div className="app">
-                <label >Wpisz kwotę:
-                    <input type="number" value={this.state.amount} onChange={this.handleChange}/>
-                </label>
-                <Dollars cash={amount} ratio ={ratioDollar}/>
-                <Euros cash={amount} ratio ={ratioEuro}/>
-            </div>
-         )
-    }
+  handleChange = e => {
+    this.setState({
+      amount: e.target.value
+    });
+  };
+  render() {
+    const { amount, ratioEuro, ratioDollar } = this.state;
+    return (
+      <div className="app">
+        <label>
+          Wpisz kwotę:
+          <input
+            type="number"
+            value={this.state.amount}
+            onChange={this.handleChange}
+          />
+        </label>
+        {/* <Dollars cash={amount} ratio ={ratioDollar}/>
+                <Euros cash={amount} ratio ={ratioEuro}/> */}
+        <Cash cash={amount} ratio={ratioDollar} title="Wartość w euro:" />
+        <Cash cash={amount} ratio={ratioEuro} title="Wartość w dolarach:" />
+      </div>
+    );
+  }
 }
- 
-ReactDOM.render(<ExchangeCounter/>, document.getElementById('root'))
+
+ReactDOM.render(<ExchangeCounter />, document.getElementById("root"));
