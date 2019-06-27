cba9cf6581fd043a8259a99076dd30a8c0e340cc
MrozKarol <polityk357@gmail.com>
2019-06-27 19:51:38 +0200

    Wip:1 optymalizacja dodanie tablicy

--- Projekty/Przelicznik walut/App.js
+++ Projekty/Przelicznik walut/App.js
@@ -5,6 +5,7 @@
 // const Euros = (props) => (
 //     <div>Wartość w euro:{props.cash <= 0 ? "" :(props.cash / props.ratio).toFixed(2)}</div>
 // )
+// optymalizacja
 
 const Cash = props => (
   <div>
@@ -16,10 +17,30 @@ const Cash = props => (
 class ExchangeCounter extends React.Component {
   state = {
     amount: "",
-    ratioDollar: 3.6,
-    ratioEuro: 4.2
+    // ratioDollar: 3.6,
+    // ratioEuro: 4.2
   };
 
+  currencies = [
+    {
+      id: 1,
+      name:"dollar",
+      ratio: 3.6,
+      title: "Wartość w dolarach:",
+    },
+    {
+      id: 2,
+      name:"euro",
+      ratio: 4.1,
+      title: "Wartość w deuro:",
+    },
+    {
+      id: 3,
+      name:"pound",
+      ratio: 5.1,
+      title: "Wartość w funtach:",
+    },
+  ]
   handleChange = e => {
     this.setState({
       amount: e.target.value
@@ -37,8 +58,6 @@ class ExchangeCounter extends React.Component {
             onChange={this.handleChange}
           />
         </label>
-        {/* <Dollars cash={amount} ratio ={ratioDollar}/>
-                <Euros cash={amount} ratio ={ratioEuro}/> */}
         <Cash cash={amount} ratio={ratioDollar} title="Wartość w euro:" />
         <Cash cash={amount} ratio={ratioEuro} title="Wartość w dolarach:" />
       </div>
