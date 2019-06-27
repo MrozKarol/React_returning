9d1157189d3b00fa79cf54b2d67cb8b4b40e98c4
MrozKarol <polityk357@gmail.com>
2019-06-27 20:08:50 +0200

    dodanie plików Przelicznik walut v0.1.0

---
+++ Projekty/Przelicznik walut v 0.1.0/App.js
@@ -0,0 +1,78 @@
+// const Dollars = (props) =>(
+//     <div>Wartość w dolarach:{props.cash <= 0 ? "" :(props.cash / props.ratio).toFixed(2)}</div>
+// )
+
+// const Euros = (props) => (
+//     <div>Wartość w euro:{props.cash <= 0 ? "" :(props.cash / props.ratio).toFixed(2)}</div>
+// )
+// optymalizacja
+
+const Cash = props => (
+  <div>
+    {props.title}
+    {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}
+  </div>
+);
+
+class ExchangeCounter extends React.Component {
+  state = {
+    amount: ""
+    // ratioDollar: 3.6,
+    // ratioEuro: 4.2
+  };
+
+  currencies = [
+    {
+      id: 1,
+      name: "dollar",
+      ratio: 3.6,
+      title: "Wartość w dolarach:"
+    },
+    {
+      id: 2,
+      name: "euro",
+      ratio: 4.1,
+      title: "Wartość w deuro:"
+    },
+    {
+      id: 3,
+      name: "pound",
+      ratio: 5.1,
+      title: "Wartość w funtach:"
+    }
+  ];
+
+  handleChange = e => {
+    this.setState({
+      amount: e.target.value
+    });
+  };
+  render() {
+    const { amount } = this.state;
+
+    const calculators = this.currencies.map(currency => (
+      <Cash
+        key={currency.id}
+        ratio={currency.ratio}
+        title={currency.title}
+        cash={amount}
+      />
+    ));
+
+    return (
+      <div className="app">
+        <label>
+          Wpisz kwotę:
+          <input
+            type="number"
+            value={this.state.amount}
+            onChange={this.handleChange}
+          />
+        </label>
+        {calculators}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ExchangeCounter />, document.getElementById("root"));
