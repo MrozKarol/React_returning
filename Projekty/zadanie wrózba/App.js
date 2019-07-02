63c381b0b0de0fe77d3d4b4ff198f0a480f47cf8
MrozKarol <polityk357@gmail.com>
2019-07-02 20:45:40 +0200

    aplikacja Zadanie wróżba

---
+++ Projekty/zadanie wrózba/App.js
@@ -0,0 +1,16 @@
+class Counter extends React.Component {
+
+    state = {
+     
+    }
+
+    render(){
+        return(
+            <React.Fragment  >
+                <h1>Zadanie wrózba</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'))
