7675eb372b2ca65e94ae44759b3eea8fbd420a99
MrozKarol <polityk357@gmail.com>
2019-07-03 21:41:27 +0200

    Wip wróxba

--- Projekty/zadanie wrózba/App.js
+++ Projekty/zadanie wrózba/App.js
@@ -4,15 +4,35 @@
 class Omen extends React.Component {
 
     state = {
-     
+     options:['1', '2', '3'],
+     option:null,
+     omen1: "bez pracy nie ma...",
+     omen2: "czym za młodu...",
+     omen3: "pokaż dup ..."
     }
 
+    
+    handleShowOmen = () => {
+        const index = Math.floor(Math.random()*this.state.options.length)
+        this.setState({
+            option: this.state.options[index]
+        });
+        
+    }
+    showOmen = () => {
+        const index = Math.floor(Math.random()*this.state.options.length)
+        if( index === "1"){
+            this.state.omen1
+        }
+    }
+        
+    
     render(){
         return(
             <React.Fragment  >
-               <button>Zobacz wróżbę</button>
-          
-               <h2>dd</h2>
+               <button onClick={this.handleShowOmen}>Zobacz wróżbę</button>
+             {this.state.option ? <h1>{this.state.option}</h1> : null}
+             {this.showOmen}
             </React.Fragment>
         )
     }
