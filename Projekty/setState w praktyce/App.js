b028c3ec420ac571c74cd0e8c77c3c6fb670a26f
MrozKarol <polityk357@gmail.com>
2019-07-04 20:46:24 +0200

    dodanie setState w praktyce, dodanie plików

---
+++ Projekty/setState w praktyce/App.js
@@ -0,0 +1,16 @@
+
+class Counter extends React.Component {
+    state = {
+
+    };
+
+    render() {
+
+        return (
+            <div className="app">c
+         </div>
+        );
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
