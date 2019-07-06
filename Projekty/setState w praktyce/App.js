20fb42295eb9ea393e358cbe47a73d6d8571c77f
MrozKarol <polityk357@gmail.com>
2019-07-06 22:31:28 +0200

    wip..

--- Projekty/setState w praktyce/App.js
+++ Projekty/setState w praktyce/App.js
@@ -1,7 +1,8 @@
 class Counter extends React.Component {
     state = {
         number1: 0,
-        number2: 0
+        number2: 0,
+        number3:0
     };
 
     handleClick1 = () => {
@@ -21,17 +22,38 @@ class Counter extends React.Component {
         this.setState({
             number2: this.state.number2 + 1
         });
-        console.log(this.state.number2 + " w metodzie handleClick2");
+        // console.log(this.state.number2 + " w metodzie handleClick2");
     };
 
+    handleClick3 =()=>{
+        this.setState((state)=>({
+            number3: this.state.number3 + 1
+        })),
+        this.setState((state)=>({
+            number3: this.state.number3 + 1
+        })),
+        this.setState((state)=>{
+           
+            return{
+                number3: this.state.number3 + 1
+            }
+            
+        }),
+
+        console.log(this.state.number3 + " w metodzie handleClick3")
+    }
+
     render() {
-        console.log(this.state.number2 + "w render");
+        // console.log(this.state.number2 + "w render handleClick2");
+        // console.log(this.state.number3 + "w render handleClick3");
         return (
             <div className="app">
                 <button onClick={this.handleClick1}>Podaj o 1</button>
                 <h2>{this.state.number1}</h2>
                 <button onClick={this.handleClick2}>Podaj o 3</button>
                 <h2>{this.state.number2}</h2>
+                <button onClick={this.handleClick3}>Dodaj o 3</button>
+                <h2>{this.state.number3}</h2>
             </div>
         );
     }
