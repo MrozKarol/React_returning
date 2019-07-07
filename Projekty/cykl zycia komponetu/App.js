faf1db6d75ee0f021e9ee1bb50225f17ed4a5df0
MrozKarol <polityk357@gmail.com>
2019-07-07 12:04:01 +0200

    dodanie xykl życia komponentu, dodanie plików html i js

---
+++ Projekty/cykl zycia komponetu/App.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+
+
+    render(){
+        return(
+            <React.Fragment  >
+               <p>montowanie komponentu</p>
+            </React.Fragment>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
