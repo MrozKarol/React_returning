4e1f589c2cfb7208e1c73e30f63cbc0aa39cd2cf
MrozKarol <polityk357@gmail.com>
2019-07-07 12:10:51 +0200

    metoda componentWillMount() która wywołuje sie sama przed render, metoda nie jest wsparana od react 17

--- Projekty/cykl zycia komponetu/App.js
+++ Projekty/cykl zycia komponetu/App.js
@@ -1,7 +1,19 @@
 class App extends React.Component {
 
+    constructor(props){
+        super(props)
+        this.state={}
+        console.log("Wywołać metode constructor")
+    }
+
+    // metoda wycofana od wersji 17
+    //metoda która wywołuje sie sama przez react
+    componentWillMount(){
+        console.log("Wywołaś componentWillMount")
+    }
 
     render(){
+        console.log("Wywołana metoda render")
         return(
             <React.Fragment  >
                <p>montowanie komponentu</p>
