1b73418685fc53908734c94626a941d190aaaae6
MrozKarol <polityk357@gmail.com>
2019-07-07 12:46:06 +0200

    dodanie Przykad.html i Przykład.js

---
+++ Projekty/cykl zycia komponetu/Przykład.js
@@ -0,0 +1,13 @@
+class Przykład extends React.Component {
+
+
+    render(){
+        return(
+            <React.Fragment  >
+               <p>montowanie komponentukkkk</p>
+            </React.Fragment>
+        )
+    }
+}
+
+ReactDOM.render(<Przykład/>, document.getElementById('root'))
