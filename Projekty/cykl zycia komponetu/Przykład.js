bc3a2ef750cb9480d440ebe94a460dee5715d266
MrozKarol <polityk357@gmail.com>
2019-07-07 13:24:14 +0200

    pokaznie na przykładzie Child1 kolejności monotowanie komponentu w react

--- Projekty/cykl zycia komponetu/Przykład.js
+++ Projekty/cykl zycia komponetu/Przykład.js
@@ -1,13 +1,49 @@
 class Przykład extends React.Component {
+    state = {
+        status: true,
+        numberUpdate: 0
+    };
+    componentDidMount() {
+        console.log("Aplikacja zamontowana")
 
 
-    render(){
-        return(
-            <React.Fragment  >
-               <p>montowanie komponentukkkk</p>
+    }
+
+    componentDidUpdate() {
+        console.log("Aplikacja zaktualizowna")
+    }
+    render() {
+        console.log("renderowanie aplikacji")
+        return (
+            <React.Fragment>
+                <button
+                    onClick={() => this.setState({ status: !this.state.status }, this.setState({ numberUpdate: this.state.numberUpdate + 1 }))}
+                    style={this.state.status ? { color: "green" } : { color: "red" }}
+                >Przełącz
+                </button>
+                <p>Liczba aktualizacji: {this.state.numberUpdate}</p>
+                <Child status={this.state.status}/>
             </React.Fragment>
+        );
+    }
+}
+
+class Child extends React.Component {
+    componentDidMount() {
+        console.log("Child zamontowany")
+    }
+
+    componentDidUpdate() {
+        console.log("Chaild zaktualizowany")
+    }
+    render(){
+        console.log("renderowanie Child1")
+        return(
+            <div style={this.props.status ? { color: "green" } : { color: "red" }}>{String(this.props.status).toLocaleUpperCase()}</div>
         )
     }
 }
 
-ReactDOM.render(<Przykład/>, document.getElementById('root'))
+
+
+ReactDOM.render(<Przykład />, document.getElementById("root"));
