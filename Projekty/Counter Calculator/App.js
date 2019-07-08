0b08c875ba230e2e68f2e82d1fe13c636368529e
MrozKarol <polityk357@gmail.com>
2019-07-08 19:54:29 +0200

    dodanie funkcji habdleMultiplication() która mnoży przez 2

--- Projekty/Counter Calculator/App.js
+++ Projekty/Counter Calculator/App.js
@@ -1,18 +1,30 @@
 class Counter extends React.Component {
-   
-    render() {
-        console.log("renderowanie Child1")
-        return (
-            <div>
-                <p>
-                    Kalkulator mnozy przez dwa, jeśli suma jest mniejsza niz 1000.Po przekroczeniu 1000 Kalkulator
-                    mnozy przez 0.5 az ościagnie sumę mniejszą niż jeden.
-                    Wtedy ponownie zaczyna mnożyć przez dwa.
-                </p>
-            </div>
-        )
-    }
-}
+  state = {
+    result: 1,
+    ratio: 2
+  };
 
+  handleMultiplication = () => {
+    this.setState({
+      result: this.state.result * this.state.ratio
+    });
+  };
+  render() {
+    console.log("renderowanie Child1");
+    return (
+      <div>
+        <p>
+          Kalkulator mnozy przez dwa, jeśli suma jest mniejsza niz 1000.Po
+          przekroczeniu 1000 Kalkulator mnozy przez 0.5 az ościagnie sumę
+          mniejszą niż jeden. Wtedy ponownie zaczyna mnożyć przez dwa.
+        </p>
+        <button onClick={this.handleMultiplication}>{`Pomnóż przez ${
+          this.state.ratio
+        }`}</button>
+        <h1>Wynik:{this.state.result}</h1>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<Counter />, document.getElementById("root"));
