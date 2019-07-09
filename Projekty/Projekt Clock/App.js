bf02ae56df7552d6fd863a22a1bab1c3149afaf0
MrozKarol <polityk357@gmail.com>
2019-07-09 21:13:16 +0200

    wyświetlenie godziny za pomocą new Date()

--- Projekty/Projekt Clock/App.js
+++ Projekty/Projekt Clock/App.js
@@ -1,13 +1,20 @@
 class Clock extends React.Component {
 
   state = {
-    time: {
-      hours: 12,
-      minutes: 15,
-      seconds: 30
-    }
+    time: this.getTime()
+  }
+  getTime(){
+    const currentTime = new Date()
+    // console.log(currentTime)
+    return({
+      // getHours() metoda wbudowana w Date, pobiera informacje z instacji Date
+      hours: currentTime.getHours(),
+      minutes: currentTime.getMinutes(),
+      seconds: currentTime.getSeconds()
+    })
   }
   render() {
+    
     const { hours, minutes, seconds } = this.state.time
     return (
       <div>
