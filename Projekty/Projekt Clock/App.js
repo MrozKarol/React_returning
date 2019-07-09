340986906da542ca1fa51b9dca415a0901fbaad5
MrozKarol <polityk357@gmail.com>
2019-07-09 20:43:26 +0200

    dodanie Projekt clock, dodanie html i js

---
+++ Projekty/Projekt Clock/App.js
@@ -0,0 +1,12 @@
+class Clock extends React.Component {
+  
+  render() {
+    return (
+      <div>
+       cloock
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById("root"));
