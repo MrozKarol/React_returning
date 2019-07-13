bbaee769dd84a664fbb2675941e6b0e9bfbf4cc6
MrozKarol <polityk357@gmail.com>
2019-07-13 20:33:45 +0200

    dodanie przycisku metody handleClick

--- ProjektyCRA/projekt-timekeeper/src/components/SwitchButton.js
+++ ProjektyCRA/projekt-timekeeper/src/components/SwitchButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const SwitchButton = (props) =>(
-    <button>Start</button>
+    <button onClick={props.onClick }>{props.active ? "Stop": "Start"}</button>
 )
 
 export default SwitchButton
