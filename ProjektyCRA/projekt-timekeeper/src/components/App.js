588e61a4f4af5de851a8241420bb12e5a69abbff
MrozKarol <polityk357@gmail.com>
2019-07-13 21:00:17 +0200

    dodanie ResetButton, przycisk który resetuje stoper

--- ProjektyCRA/projekt-timekeeper/src/components/App.js
+++ ProjektyCRA/projekt-timekeeper/src/components/App.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import SwitchButton from './SwitchButton'
+import ResetButton from './ResetButton'
 
 class App extends Component {
   state = {
     time: 0,
     active: false,
   }
-  
+
 
   handleClick = () => {
     if (this.state.active) {
       clearInterval(this.idInterval)
     } else {
-      this.idInterval = setInterval(()=>this.addSecond(),1000)
+      this.idInterval = setInterval(() => this.addSecond(), 1000)
     }
     this.setState({
       active: !this.state.active
     })
   }
-  addSecond = ()=>{
+  addSecond = () => {
     this.setState({
       time: this.state.time + 1
+    })
+  }
+
+  handleResetButoon = () => {
+    const temp = this.state.time;
+
+    this.setState({
+      time: this.state.time - temp,
+
     })
   }
 
@@ -30,6 +40,7 @@ class App extends Component {
       <div>
         <p>{this.state.time}</p>
         <SwitchButton onClick={this.handleClick} active={this.state.active} />
+        <ResetButton onClick={this.handleResetButoon} active={this.state.active} />
       </div>
     );
   }
