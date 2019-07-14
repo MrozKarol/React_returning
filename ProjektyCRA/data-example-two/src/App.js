5b868602e8987265867eabf8f1e200911aaf2dc3
MrozKarol <polityk357@gmail.com>
2019-07-14 11:39:41 +0200

    użycie componentWillUnmount

--- ProjektyCRA/data-example-two/src/App.js
+++ ProjektyCRA/data-example-two/src/App.js
@@ -1,46 +1,62 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 //profesjonalna baza danych ;)
-const data =[
-  {id:1, title:'Wiadomość numer 1', body:'Zawartość wiadomości numer 1...'},
-  {id:2, title:'Wiadomość numer 2', body:'Zawartość wiadomości numer 2...'},
+const data = [
+  { id: 1, title: 'Wiadomość numer 1', body: 'Zawartość wiadomości numer 1...' },
+  { id: 2, title: 'Wiadomość numer 2', body: 'Zawartość wiadomości numer 2...' },
 ]
 
-setInterval(()=>{
+setInterval(() => {
   //długość tablicy + 1
-const index = data.length + 1
-//pushowanie obiektu do tablicy
-data.push({
-  id:index,
-  title:`Wiadomość numer 2 ${index}`,
-  body:`Zawartość wiadomości numer ${index}...`
-})
-//Wyświetlenie tablicy w consoli
-console.log(data)
-},2000)
+  const index = data.length + 1
+  //pushowanie obiektu do tablicy
+  data.push({
+    id: index,
+    title: `Wiadomość numer ${index}`,
+    body: `Zawartość wiadomości numer ${index}...`
+  })
+  //Wyświetlenie tablicy w consoli
+  // console.log(data)
+}, 5000)
 
 class App extends Component {
-  state = { 
+  state = {
     //uzycie rest operatora dla skopiowania tablicy, samo odwołanie do tablicy data zrobiłoby referencje
     comments: [...data]
-   }
+  }
 
-  render() { 
-    const comments = this.state.comments.map(comment =>(
+  //metoda która pobiera dane
+  getData = () =>{
+    this.setState({
+      comments: [...data]
+    })
+  }
+
+  //componentDidMount() uruchaia sie tylko raz po renderze, setIrerfal chodzi cały czas wykonuje sie co 3 sekundy
+  componentDidMount(){
+    this.idiI =setInterval(this.getData, 3000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.idiI)
+  }
+
+  render() {
+    const comments = this.state.comments.map(comment => (
       //uzycie key
-      <div key={comment.id}> 
+      <div key={comment.id}>
         <h3>{comment.title}</h3>
         <h4>{comment.body}</h4>
       </div>
     ))
 
-    return ( 
+    return (
       //reverse() odwrotne umieszczenie elementów tablicy
       <div className="App">{comments.reverse()}</div>
-     );
+    );
   }
 }
- 
+
 export default App;
 
 
