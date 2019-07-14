69d73f822af3781b57922b6ec154d95143c71343
MrozKarol <polityk357@gmail.com>
2019-07-14 11:08:47 +0200

    dodanie bazy danych, uzycue setInterfal które co 2 sekundy puchuje do tablicy nowy obiekt

--- ProjektyCRA/data-example-two/src/App.js
+++ ProjektyCRA/data-example-two/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
+//profesjonalna baza danych ;)
+const data =[
+  {id:1, title:'Wiadomość numer 1', body:'Zawartość wiadomości numer 1...'},
+  {id:2, title:'Wiadomość numer 2', body:'Zawartość wiadomości numer 2...'},
+]
+
+setInterval(()=>{
+  //długość tablicy + 1
+const index = data.length + 1
+//pushowanie obiektu do tablicy
+data.push({
+  id:index,
+  title:`Wiadomość numer 2 ${index}`,
+  body:`Zawartość wiadomości numer ${index}...`
+})
+//Wyświetlenie tablicy w consoli
+console.log(data)
+},2000)
 
 function App() {
   return (
     <div className="App">
-     Działa
+     Działalll
     </div>
   );
 }
