cdc083941984e0d837c386f83c4dba117e322e64
MrozKarol <polityk357@gmail.com>
2019-07-15 18:56:12 +0200

    dodanie pliku word.json, koniecznie w pliku public

--- ProjektyCRA/fetch-local-json/src/components/App.js
+++ ProjektyCRA/fetch-local-json/src/components/App.js
@@ -1,13 +1,13 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
+  state = {}
+  render() {
+    return (
       <div className="App">fetch</div>
-     );
+    );
   }
 }
- 
+
 export default App;
