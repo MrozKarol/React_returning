e640b055c8821a2b588a92abe0d504ffc56f4d3b
MrozKarol <polityk357@gmail.com>
2019-07-15 19:44:29 +0200

    pobrnie danych za pomocą metody fetch()

--- ProjektyCRA/fetch-local-json/src/components/App.js
+++ ProjektyCRA/fetch-local-json/src/components/App.js
@@ -4,25 +4,19 @@ import Word from './Word';
 
 class App extends Component {
   state = {
-    // SYMULACJA POBRANIA DANYCH
-    words: [
-      {
-        "id": 1,
-        "en": "cat",
-        "pl": "kocieł"
-      },
-      {
-        "id": 2,
-        "en": "dog",
-        "pl": "pieseł"
-      },
-      {
-        "id": 3,
-        "en": "fish",
-        "pl": "rybka"
-      }
-    ]
+    words: []
   }
+
+  componentDidMount() {
+    fetch('data/word.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          words: data.words
+        })
+      })
+  }
+
   render() {
     const words = this.state.words.map(word => (
       <Word key={word.id} english={word.en} polish={word.pl} />
