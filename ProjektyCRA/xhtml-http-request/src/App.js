f41f5f20c836743f2e203c62e8e4be3bc8720845
MrozKarol <polityk357@gmail.com>
2019-07-16 19:25:36 +0200

    pobranie danych za pomocą XMLHttpRequest, uzycie metody openm send, onload, wyświetlenie zawartości w console.log

--- ProjektyCRA/xhtml-http-request/src/App.js
+++ ProjektyCRA/xhtml-http-request/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    const xhr = new XMLHttpRequest(); /*tworzenie instanicji obiektu*/
+    //metoda open która otwiera połączenie,GET: określenie metody, określenie adresu, 
+    //true- czy połączenie ma być asynchroniczne
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+    //send()wysłanie w nawiasie null 'nic pozatym co napisaliśmy'
+    xhr.send(null);
+    //onload: co ma sie wydarzyć w pomencie kiedy otrzymamy odpowiedź od serwera
+    xhr.onload = () => {
+      console.log(xhr.status) //sprawdzenie statusu
+      console.log(xhr.response) // sprawdzenie odpowiedzi
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        j
     </div>
-  );
+    );
+  }
 }
 
 export default App;
