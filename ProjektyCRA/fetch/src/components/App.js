8f6dca94812c7079e895b78cc285c6c530f083f7
MrozKarol <polityk357@gmail.com>
2019-07-17 20:04:59 +0200

    pobranie danych za pomocą fetch i wyświetlenie w console.log po kliknięciu w przycisk

--- ProjektyCRA/fetch/src/components/App.js
+++ ProjektyCRA/fetch/src/components/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import UsersList from './UsersList'
 import ButtonFetchUsers from './ButtonFetchUsers'
 
-const API = "http://randomuser.me/api/?results=5"
+const API = "http://randomuser.me/api/?results=5";
+const proxyurl = "https://cors-anywhere.herokuapp.com/" //rozwiazanie problemu z 'Access-Control-Allow-Origin'
 
 class App extends Component {
   state = {
     users:null
   }
   handleDataFetch = () =>{
-    console.log("click")
+    // console.log("click")
+    fetch(proxyurl + API) //fetchowanie API które zwraca promisa
+    //tchen czyli później , sprawdzenie warunkowe statusu obiektu response
+      .then(response =>{
+        if(response.text){
+          return response;
+        }//jak będzie zły status wyświetli status zły
+        throw Error(response.ok)
+      })
+      //jak jest wszystko ok bierzemy responsa i przy uzyciu json() wyciąamy go i zamieniamy na stringa
+      .then(response => response.json())
+      //nasze dane 'string' przypisujemy do users w stjcie za pomoca this.setState
+      .then(data =>{
+        this.setState({
+          users: data.results
+        })
+      })
+      //jak coś sie wykrzaczy pokazuje komunikat
+      .catch(error => console.log(error + " coś nie tak "))
+
   }
   render() {
     const users = this.state.users
