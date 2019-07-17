562507fed98904cf07baa9c3f853521712db434b
MrozKarol <polityk357@gmail.com>
2019-07-17 20:30:40 +0200

    wyświetenie listy uzytkowników w komponecie UsersList za pomocą metody map

--- ProjektyCRA/fetch/src/components/UsersList.js
+++ ProjektyCRA/fetch/src/components/UsersList.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const UsersList = (props) => {
-    console.log(props.users)
+    // console.log(props.users)
+    const users = props.users.map(user =>(
+        <div key={user.login.uuid}>
+            <img src={user.picture.thumbnail} alt=""/>
+            <h2>{`${user.name.title} ${user.name.first } ${user.name.last }`}</h2>
+            <p>Age: {user.dob.age}</p>
+            <a href={user.email}>{user.email}</a>
+        </div>
+    ))
     return (
         <ul>
-            <li>Uzytkownik przykładowy 1</li>
+          {users}
         </ul>
     );
 }
