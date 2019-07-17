be01f5ca3db3ddb722506b764beb895e329e4db5
MrozKarol <polityk357@gmail.com>
2019-07-17 20:36:08 +0200

    dodanie styli css

--- ProjektyCRA/fetch/src/components/UsersList.js
+++ ProjektyCRA/fetch/src/components/UsersList.js
@@ -4,7 +4,7 @@ const UsersList = (props) => {
     // console.log(props.users)
     const users = props.users.map(user =>(
         <div key={user.login.uuid}>
-            <img src={user.picture.thumbnail} alt=""/>
+            <img src={user.picture.thumbnail} alt="k" style={{border: '1px solid red'}} />
             <h2>{`${user.name.title} ${user.name.first } ${user.name.last }`}</h2>
             <p>Age: {user.dob.age}</p>
             <a href={user.email}>{user.email}</a>
