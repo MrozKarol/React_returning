73eabdb38d25697c29a0df5ac7374740d742918c
MrozKarol <polityk357@gmail.com>
2019-07-18 17:51:19 +0200

    dodanie css

--- ProjektyCRA/fetch-2/src/components/UsersList.js
+++ ProjektyCRA/fetch-2/src/components/UsersList.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import './UsersList.css'
 
 const UsersList = (props) => {
     // console.log(props.users)
     const users = props.users.map(user =>(
         <div key={user.login.uuid}>
-            <img src={user.picture.thumbnail} alt="k" style={{border: '1px solid red'}} />
+            <img src={user.picture.large} alt="k" 
+            style={{border: '1px solid red'}} />
             <h2>{`${user.name.title} ${user.name.first } ${user.name.last }`}</h2>
             <p>Age: {user.dob.age}</p>
             <a href={user.email}>{user.email}</a>
         </div>
     ))
     return (
-        <ul>
+        <div className="users">
           {users}
-        </ul>
+        </div>
     );
 }
 
