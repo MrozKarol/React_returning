558dd69c79818ba4c35308a1e5bdf09267b036de
MrozKarol <polityk357@gmail.com>
2019-07-18 18:26:57 +0200

    pojedyncze dodawanie uzytkowników za pomocą dodwania do tablicy metoda concat

--- ProjektyCRA/fetch-2/src/components/App.js
+++ ProjektyCRA/fetch-2/src/components/App.js
@@ -8,14 +8,14 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/" //rozwiazanie problemu z
 
 class App extends Component {
   state = {
-    users:[]
+    users: []
   }
-  handleDataFetch = () =>{
+  handleDataFetch = () => {
     // console.log("click")
     fetch(proxyurl + API) //fetchowanie API które zwraca promisa
-    //tchen czyli później , sprawdzenie warunkowe statusu obiektu response
-      .then(response =>{
-        if(response.text){
+      //tchen czyli później , sprawdzenie warunkowe statusu obiektu response
+      .then(response => {
+        if (response.text) {
           return response;
         }//jak będzie zły status wyświetli status zły
         throw Error(response.ok)
@@ -23,10 +23,12 @@ class App extends Component {
       //jak jest wszystko ok bierzemy responsa i przy uzyciu json() wyciąamy go i zamieniamy na stringa
       .then(response => response.json())
       //nasze dane 'string' przypisujemy do users w stjcie za pomoca this.setState
-      .then(data =>{
-        this.setState({
-          users: data.results
-        })
+      .then(data => {
+        const user = data.results;
+
+        this.setState(prevState => ({
+          users: prevState.users.concat(user)
+        }))
       })
       //jak coś sie wykrzaczy pokazuje komunikat
       .catch(error => console.log(error + " coś nie tak "))
@@ -37,7 +39,7 @@ class App extends Component {
     return (
       <div>
         <ButtonFetchUsers click={this.handleDataFetch} />
-        {users ? <UsersList users={users} /> : users}
+        {users.length >0 ? <UsersList users={users} /> : users}
       </div>
     );
   }
