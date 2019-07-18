9ed4c0773ebbf12786175436a5e5fdbea7ad0191
MrozKarol <polityk357@gmail.com>
2019-07-18 17:34:48 +0200

    przygotowanie struktury, baza

---
+++ ProjektyCRA/fetch-2/src/components/ButtonFetchUsers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ButtonFetchUsers = (props) => {
+    return (
+        <button onClick={props.click}>Dodaj 5 uzytkowników</button>
+    );
+}
+
+export default ButtonFetchUsers;
---
+++ ProjektyCRA/fetch-2/src/components/UsersList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const UsersList = (props) => {
+    // console.log(props.users)
+    const users = props.users.map(user =>(
+        <div key={user.login.uuid}>
+            <img src={user.picture.thumbnail} alt="k" style={{border: '1px solid red'}} />
+            <h2>{`${user.name.title} ${user.name.first } ${user.name.last }`}</h2>
+            <p>Age: {user.dob.age}</p>
+            <a href={user.email}>{user.email}</a>
+        </div>
+    ))
+    return (
+        <ul>
+          {users}
+        </ul>
+    );
+}
+
+export default UsersList;
