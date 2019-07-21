c67ee2b725490d86d60ef0d4de03e5b87bcd554b
MrozKarol <polityk357@gmail.com>
2019-07-21 19:20:20 +0200

    dodanie formularza Twoje imie i metody handleChange do obsługi

--- ProjektyCRA/from-with-validation/src/App.js
+++ ProjektyCRA/from-with-validation/src/App.js
@@ -1,12 +1,28 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {}
+  state = {
+    username: '',
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.type);
+    console.log(e.target.name);
+    const value = e.target.value;
+    this.setState({
+      username: value
+    })
+  }
+
   render() {
     return (
-      <div>
-        działa
+      <div className="App">
+        <form>
+          <label htmlFor="user">Twoje imię:
+            <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange} />
+          </label>
+        </form>
       </div>);
   }
 }
