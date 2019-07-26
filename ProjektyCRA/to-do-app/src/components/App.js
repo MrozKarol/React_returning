36ec89f38d812d843fc37250669c0c312528ed6d
MrozKarol <polityk357@gmail.com>
2019-07-26 20:37:46 +0200

    dodanie state, pojedyncze taski task:tablica z obiektami, id unikalne do mapowania, text nazwa zadania: important czy jest wazne, acive czy jest aktywne, finishDate data zakonczenia

--- ProjektyCRA/to-do-app/src/components/App.js
+++ ProjektyCRA/to-do-app/src/components/App.js
@@ -5,14 +5,41 @@ import TaskList from './TaskList'
 
 
 class App extends Component {
-  state = {}
+  state = {
+    task: [
+      {
+        id: 0,
+        text: 'zagrać w gre Tomb Rider :)',
+        date: '2019-02-15',
+        important: true,
+        active: true,
+        finishDate: null,
+      },
+      {
+        id: 2,
+        text: 'nauczyć sie reacta :)',
+        date: '3001-01-01',
+        important: true,
+        active: true,
+        finishDate: null,
+      },{
+        id: 3,
+        text: 'pozdrowić Damiana :)',
+        date: '2020-01-01',
+        important: false,
+        active: true,
+        finishDate: null,
+      }
+
+    ]
+  }
   render() {
     return (
-    <div>
-      działa
-      <AddTask/>
-      <TaskList/>
-    </div>);
+      <div>
+        działa
+      <AddTask />
+        <TaskList />
+      </div>);
   }
 }
 
