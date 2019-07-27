fb0430646d91b307684129242e39656f81fa0270
MrozKarol <polityk357@gmail.com>
2019-07-27 22:07:31 +0200

    aktualizacja pojedynczego tasku, taski important kolorem czerownym, taski zrobione dodanie daty i godziny zakończenia

--- ProjektyCRA/to-do-app/src/components/Task.js
+++ ProjektyCRA/to-do-app/src/components/Task.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
 const Task = (props) => {
+    const importantStle = {
+        color: 'red',
+    }
     //destrukteryzacja wyciągnięcie z props task patrz Task List
-    const { text, date, id } = props.task
-    return (
-        <div>
-            <p>
-                <strong>{text}</strong> - do  <span>{date} </span>
-                <button onClick={() => props.change(id)}>Zostało zrobione</button>
-                <button onClick={() => props.delate(id)}>X</button>
-            </p>
-        </div>
-    );
+    const { text, date, id, active, important, finishDate } = props.task;
+    if (active === true) {
+        return (
+            <div>
+                <p>
+                    <strong style={important ? importantStle : null}>{text}</strong> - do  <span>{date} </span>
+                    <button onClick={() => props.change(id)}>Zostało zrobione</button>
+                    <button onClick={() => props.delate(id)}>X</button>
+                </p>
+            </div>
+        );
+    } else {
+        //wyodrębnienie godziny i daty
+        const finish = new Date(finishDate).toLocaleString()
+        return (
+            <div>
+                <p>
+                    <strong >{text}</strong> - do  <em>{date} </em>
+                    <br />
+                    -potweirdzenie wykonania {finish}
+
+                    <button onClick={() => props.delate(id)}>X</button>
+                </p>
+            </div>
+        );
+    }
+
 }
 
 export default Task;
