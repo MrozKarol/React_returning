95cf31cbb49fbfd38afd2e557ccf9fb995938234
MrozKarol <polityk357@gmail.com>
2019-08-06 18:13:11 +0200

    użycie Route

--- React-Router/projekt-spa/src/App.js
+++ React-Router/projekt-spa/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
+
+const Home = () => <h1>Strona startowa</h1>
+const News = () => <h1>Aktualności</h1>
+const Contact = () => <h1>Kontakt do nas</h1>
+
 
 function App() {
   return (
@@ -12,13 +17,15 @@ function App() {
             <ul>
               <li><Link to="/">Start</Link></li>
               <li><Link to="/news">aktualności</Link></li>
-              <li><Link to="/concat">kontakt</Link></li>
+              <li><Link to="/contact">kontakt</Link></li>
             </ul>
           </nav>
         </header>
         <section>
-          Siemanko :)
-      </section>
+         <Route path="/" component={Home}/>
+         <Route path="/news" component={News}/>
+         <Route path="/contact" component={Contact}/>
+        </section>
       </div>
     </BrowserRouter>
 
