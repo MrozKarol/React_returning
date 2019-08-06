b47800a84a568a593b1a890126ce05a58e1f0f5e
MrozKarol <polityk357@gmail.com>
2019-08-06 17:55:36 +0200

    dodanie struktury

--- React-Router/projekt-spa/src/App.js
+++ React-Router/projekt-spa/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css'
+
 function App() {
   return (
     <div>
-      React Router
+      <header>
+        <nav>
+          <ul>
+            <li><a href="/">Start</a></li>
+            <li><a href="/news">aktualności</a></li>
+            <li><a href="/concat">kontakt</a></li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        Siemanko :)
+      </section>
     </div>
   );
 }
