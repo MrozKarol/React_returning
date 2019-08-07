0d567c23751e2c87c27c750d6986a4a86462ad74
MrozKarol <polityk357@gmail.com>
2019-08-07 20:41:10 +0200

    instalacja react router i import

--- React-Router/projekt2-spa/src/layouts/App.js
+++ React-Router/projekt2-spa/src/layouts/App.js
@@ -1,15 +1,19 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/App.css';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <div className="App">
-      Działa
-     </div>
-     );
+  state = {}
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          Działa
+        </div>
+      </Router>
+
+    );
   }
 }
- 
+
 export default App;
