2efa5974928f66e9459d5568b713c356172b838d
MrozKarol <polityk357@gmail.com>
2019-08-07 21:15:29 +0200

    wip: nav

--- React-Router/projekt2-spa/src/layouts/Navigation.js
+++ React-Router/projekt2-spa/src/layouts/Navigation.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 const Navigation = () => {
     return ( 
-        <div>Navigation</div>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Start</NavLink>
+                </li>
+                <li>
+                    <NavLink to="products">Produkty</NavLink>
+                </li>
+                <li>
+                    <NavLink to="contakt">Kontakt</NavLink>
+                </li>
+                <li>
+                    <NavLink to="admin">Panel admina</NavLink>
+                </li>
+            </ul>
+        </nav>
      );
 }
  
