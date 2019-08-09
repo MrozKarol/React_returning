5c63438f8f7a0aa3024dfb1eab7cb97570cc8ace
MrozKarol <polityk357@gmail.com>
2019-08-09 18:40:15 +0200

    dodanie routingu do głównej strony

--- React-Router/projekt2-spa/src/layouts/Page.js
+++ React-Router/projekt2-spa/src/layouts/Page.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
-import HomePage from '../pages/HomePage'
+import { Route , Switch } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage';
+import ProductPage from '../pages/ProductsPage';
+import ContactPage from '../pages/ContactPage';
+import AdminPage from '../pages/AdminPage';
+import ErrorPage from '../pages/ErrorPage';
+
+
 const Page = () => {
-    return ( 
-        <Route path="/" exact component ={HomePage}/>
-     );
+    return (
+        <>
+            <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/contacts" component={ContactPage} />
+            <Route path="/products" component={ProductPage} />
+            <Route path="/admin" component={AdminPage} />
+            <Route component={ErrorPage} />
+            </Switch>
+            
+        </>
+    );
 }
- 
+
 export default Page;
