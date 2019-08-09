1702e1b3a7f95018ce88944b4c060a3d753d6074
MrozKarol <polityk357@gmail.com>
2019-08-09 17:34:31 +0200

    dodanie grafik header i wyświetlenie

--- React-Router/projekt2-spa/src/layouts/Header.js
+++ React-Router/projekt2-spa/src/layouts/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+
+import img1 from '../images/header1.jpg'
+import img2 from '../images/header2.jpg'
+import img3 from '../images/header3.jpg'
+
 const Header = () => {
     return ( 
-        <div>Header</div>
+        <div>
+            <img src={img1} alt=""/>
+        </div>
      );
 }
  
