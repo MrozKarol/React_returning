4c1177647fce007b0e04ed5e6ce1eee2072da57e
MrozKarol <polityk357@gmail.com>
2019-08-09 19:05:22 +0200

    dodanie Article , zaiportowanie Article do HomePage

--- React-Router/projekt2-spa/src/pages/HomePage.js
+++ React-Router/projekt2-spa/src/pages/HomePage.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import Article from '../components/Article'
 
+const articles = [
+    {
+        id: 1,
+        title: "Czym jest React",
+        author: "Arnold B",
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus omnis tenetur, officia deleniti voluptate fugit tempore unde velit dignissimos laboriosam, alias ipsa error vel nihil nemo accusantium impedit, itaque magnam!'
+    },
+    {
+        id: 2,
+        title: "Czy warto uczyć sie React ?",
+        author: "dziadek Arnolda B",
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus omnis tenetur, officia deleniti voluptate fugit tempore unde velit dignissimos laboriosam, alias ipsa error vel nihil nemo accusantium impedit, itaque magnam!'
+    },
+    {
+        id: 3,
+        title: "Czy warto ...? ",
+        author: "pra pra dziadek Arnolda B",
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus omnis tenetur, officia deleniti voluptate fugit tempore unde velit dignissimos laboriosam, alias ipsa error vel nihil nemo accusantium impedit, itaque magnam!'
+    }
+]
 const HomePage = () => {
-    return ( <div>Strona Strartowa</div> );
+    const articleList = articles.map(article =>(
+        <Article key={article.id} {...article}/>
+    ))
+    return (
+        <div className="home">
+            {articleList}
+        </div>
+    );
 }
- 
+
 export default HomePage;
