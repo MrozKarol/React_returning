5e817ed4cb4243971989904fd74a720f3ee8bc93
MrozKarol <polityk357@gmail.com>
2019-08-09 19:32:58 +0200

    dodanie formularza, dodanie metody handleHange która obsługuje wpisywanie, dodanie handleSubmit która wyłacza domyślne działanie i resetuje value po wysłaniu

--- React-Router/projekt2-spa/src/pages/ContactPage.js
+++ React-Router/projekt2-spa/src/pages/ContactPage.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
 
 class ContactPage extends Component {
-    state = {  }
+    state = { 
+        value: ''
+     }
+
+     handleSubmit = (e) =>{
+         e.preventDefault()
+         this.setState({
+             value: ''
+         })
+     }
+     handleHange = (e) =>{
+         this.setState({
+             value: e.target.value
+         })
+     }
     render() { 
         return ( 
-            <div>Co</div>
+            <div className="contact">
+                <form onSubmit={this.handleSubmit} >
+                    <h3>Napisz do nas:</h3>
+                    <textarea value={this.state.value} onChange={this.handleHange} placeholder='Wpisz wiadomość'></textarea>
+                    <button >wyślij</button>
+                </form>
+            </div>
          );
     }
 }
