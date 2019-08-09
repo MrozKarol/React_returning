0abf9891d67d8d57a4dcfe9e30f356887237aca4
MrozKarol <polityk357@gmail.com>
2019-08-09 18:00:02 +0200

    skonczenie header

--- React-Router/projekt2-spa/src/layouts/Header.js
+++ React-Router/projekt2-spa/src/layouts/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
 import '../styles/Header.css';
+
 
 import img1 from '../images/header1.jpg'
 import img2 from '../images/header2.jpg'
@@ -8,7 +10,18 @@ import img3 from '../images/header3.jpg'
 const Header = () => {
     return ( 
         <div>
-            <img src={img2} alt=""/>
+            <Route path="/" exact render={()=>(
+                <img src={img3} alt="miasto"/>
+            )}/>
+            <Route path="/products" render={()=>(
+                <img src={img1} alt="miasto"/>
+            )}/>
+            <Route path="/contacts" render={()=>(
+                <img src={img2} alt="miasto"/>
+            )}/>
+            <Route path="/admin" render={()=>(
+                <img src={img1} alt="miasto"/>
+            )}/>
         </div>
      );
 }
