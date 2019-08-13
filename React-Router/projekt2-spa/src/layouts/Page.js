38c590a81460857fa46eada8c3aaabe2748181e5
MrozKarol <polityk357@gmail.com>
2019-08-13 20:58:29 +0200

    dodanie ProductListPage.js, zaiporotwanie i dodanie do Rauta w Page.js

--- React-Router/projekt2-spa/src/layouts/Page.js
+++ React-Router/projekt2-spa/src/layouts/Page.js
@@ -3,6 +3,7 @@ import { Route , Switch } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
 import ProductPage from '../pages/ProductsPage';
+import ProductListPage from '../pages/ProductListPage'
 import ContactPage from '../pages/ContactPage';
 import AdminPage from '../pages/AdminPage';
 import LoginPage from '../pages/LoginPage';
@@ -15,7 +16,7 @@ const Page = () => {
             <Switch>
             <Route path="/" exact component={HomePage} />
             <Route path="/contacts" component={ContactPage} />
-            <Route path="/products" component={ProductPage} />
+            <Route path="/products" component={ProductListPage} />
             <Route path="/admin" component={AdminPage} />
             <Route path="/login" component={LoginPage} />
             <Route component={ErrorPage} />
---
+++ React-Router/projekt2-spa/src/pages/ProductListPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductListPage = () => {
+    return ( 
+        <div>Lista Produktów</div>
+     );
+}
+ 
+export default ProductListPage;
