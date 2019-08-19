a0a836191825a455a3810595d4405de8d4b9a35f
MrozKarol <polityk357@gmail.com>
2019-08-19 19:49:15 +0200

    dodanie przycisku

--- StyleReact/style-inline/src/App.js
+++ StyleReact/style-inline/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = { 
+    active: false
+   }
 
+   handleClick = () =>{
+     this.setState({
+       active: !this.state.active
+     })
+   }
+
+  render() { 
+    return ( 
+      <div>
+        <button onClick={this.handleClick}>{this.state.active ? 'Wyłacz' : 'Włacz'}</button>
+      </div>
+    );
+  }
+}
+ 
 export default App;
