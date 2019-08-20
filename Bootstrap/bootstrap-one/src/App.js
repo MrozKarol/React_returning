7fa262e9272779b9189de77c4e5df44efdc66221
MrozKarol <polityk357@gmail.com>
2019-08-20 19:47:19 +0200

    dodanie Leyout.js i uzycie Container

--- Bootstrap/bootstrap-one/src/App.js
+++ Bootstrap/bootstrap-one/src/App.js
@@ -4,18 +4,21 @@ import Home from '../src/pages/Home';
 import About from '../src/pages/About';
 import Contact from '../src/pages/Concat';
 import NoMatch from '../src/pages/NoMatch'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
     </React.Fragment>
   );
 }
