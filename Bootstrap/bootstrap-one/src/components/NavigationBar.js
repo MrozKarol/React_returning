c5677fab5257e3194c1e88d9361e004b100789b7
MrozKarol <polityk357@gmail.com>
2019-08-20 20:22:07 +0200

    Wip

--- Bootstrap/bootstrap-one/src/components/NavigationBar.js
+++ Bootstrap/bootstrap-one/src/components/NavigationBar.js
@@ -3,33 +3,37 @@ import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components'
 
 
-const Styled = styled.div`
+const Styles = styled.div`
     .navbar{
-        bacground-color: #222;   
+        background-color: #222;   
     }
-    .navbar-brand, .navbrar-nav . nav-link{
-        color: #bbb
+    .navbar-brand, .navbrar-nav, .nav-link{
+        color: orange;
+
+        &:hover{
+            color:white;
+        }
     }
-    .navbar-brand, .navbrar-nav . nav-link:hover{
-        color: red
-    }
+
+    
+    
 `;
 
 const NavigationBar = () => {
     return (
-        <Styled>
+        <Styles>
             <Navbar expand="lg">
                 <Navbar.Brand href="/">Code</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
+                    <Nav className="ml-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/about">About</Nav.Link>
                         <Nav.Link href="/contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-        </Styled>
+        </Styles>
     );
 }
 
