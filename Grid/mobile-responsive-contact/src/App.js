d0e0887270d47d7bdf854c3f5a2bd5ea7952e698
MrozKarol <polityk357@gmail.com>
2019-08-22 18:45:06 +0200

    dodanie wersji mobilnej

--- Grid/mobile-responsive-contact/src/App.js
+++ Grid/mobile-responsive-contact/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1 className="brand">Web Desing</h1>
+      <h1 className="brand"><span>Web Desing</span></h1>
       <div className="wrapper">
         <div className="company-info">
         <h3>Web Desing</h3>
@@ -14,7 +14,7 @@ function App() {
           <li>test@gmail.com</li>
         </ul>
         </div>
-        <dic className="contact">
+        <div className="contact">
           <h3>Email Us</h3>
           <form action="">
             <p>
@@ -26,10 +26,6 @@ function App() {
               <input type="text" name="company" id=""/>
             </p>
             <p>
-              <label htmlFor="">Name</label>
-              <input type="text" name="name" id=""/>
-            </p>
-            <p>
               <label htmlFor="">Email</label>
               <input type="email" name="email" id=""/>
             </p>
@@ -37,16 +33,15 @@ function App() {
               <label htmlFor="">Phone Number</label>
               <input type="number" name="phone" id=""/>
             </p>
-            <p>
+            <p className="full">
               <label htmlFor="">Mesage</label>
               <textarea type="text" name="mesage" rows="5" id=""></textarea>
             </p>
-            <p>
+            <p className="full">
               <button>Submit</button>
             </p>
           </form>
-
-        </dic>
+        </div>
       </div>
      
     </div>
