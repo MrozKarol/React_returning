bcbd71646b6eae523934c6288c99c7fcc842344c
MrozKarol <polityk357@gmail.com>
2019-08-26 17:50:47 +0200

    dodanie about.js i service.js

---
+++ Gatsby/gatsby-one/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const AboutPage  = () => {
+    return ( 
+        <div>
+            <h1>Strona o mnie</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid, maxime saepe, iste obcaecati magnam, ab odit optio pariatur autem repellat eveniet. Neque itaque at veniam voluptatum excepturi omnis facere.</p>
+        </div>
+     );
+}
+ 
+export default AboutPage;
---
+++ Gatsby/gatsby-one/src/pages/service.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServicesPage = () => {
+    return ( 
+        <div>
+            <h1>ServicesPage</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, non.</p>
+        </div>
+     );
+}
+ 
+export default ServicesPage;
