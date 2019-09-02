2b07203709eab9e242c6050577eea511496d81ec
MrozKarol <polityk357@gmail.com>
2019-09-02 12:06:45 +0200

    add bootstrapnavbar

--- Bootstrap/bootstrap-fake/src/components/NavbarCustom.js
+++ Bootstrap/bootstrap-fake/src/components/NavbarCustom.js
@@ -1,52 +1,33 @@
 import React, { Component } from "react";
-import './NavbarCustom.css'
-import {
-  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse,
-} from "mdbreact";
-import { BrowserRouter as Router } from 'react-router-dom';
-import 'mdbreact/dist/css/mdb.css';
+import './NavbarCustom.css';
+import { Nav,Navbar } from 'react-bootstrap';
+
 
 import logoApp from '../img/logo.png'
 
-
-
 class NavbarCoustom extends Component {
-  state = {
-    isOpen: false
-  };
 
-  toggleCollapse = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  }
 
   render() {
     return (
-      <Router>
-        <MDBNavbar color=" blue-grey lighten-5" dark expand="lg">
-          <MDBNavbarBrand>
-            <span className="white-text"><img src={logoApp} alt="" /></span>
-          </MDBNavbarBrand>
-          <MDBNavbarToggler onClick={this.toggleCollapse} />
-          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-            <MDBNavbarNav left>
-              <MDBNavItem active>
-                <MDBNavLink className="black-text text-uppercase" to="/home">Home</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink className="black-text text-uppercase" to="/about">About</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink className="black-text text-uppercase" to="/serwices">Serwices</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-
-              </MDBNavItem>
-            </MDBNavbarNav>
-            <MDBNavbarNav left>
-            </MDBNavbarNav>
-          </MDBCollapse>
-        </MDBNavbar>
-      </Router>
+      
+        <Navbar collapseOnSelect expand="md" bg="light">
+      <Navbar.Brand href="/"><img src={logoApp} alt=""/></Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+          
+          <Nav.Link href="#features">Home</Nav.Link>
+          <Nav.Link href="#pricing">Do About</Nav.Link>
+          <Nav.Link href="#pricing">Services</Nav.Link>
+          <Nav.Link href="#pricing">Team</Nav.Link>
+          <Nav.Link href="#pricing">Connect</Nav.Link>
+         
+        </Nav>
+  
+      </Navbar.Collapse>
+    </Navbar>
+      
     );
   }
 }
