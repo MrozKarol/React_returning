3dc3a5bcc95a32708dc80b7ce928d218b0da8669
MrozKarol <polityk357@gmail.com>
2019-10-01 18:30:17 +0200

    used two context in two different component

--- Contex_Hooks/contextapp/src/App.js
+++ Contex_Hooks/contextapp/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import BookList from './components/BookList';
-import ThemeContextProvider from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import BookList from "./components/BookList";
+import ThemeContextProvider from "./contexts/ThemeContext";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-      <Navbar />
-      <BookList />
-      <ThemeToggle/>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
 }
 
 export default App;
+ 
