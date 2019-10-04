dde2c5504cedb050d8d01388d7a907bd91736a3c
MrozKarol <polityk357@gmail.com>
2019-10-04 18:33:17 +0200

    used to different hook with to different render by using useEffect

--- Contex_Hooks/hooksapp/src/components/SongList.js
+++ Contex_Hooks/hooksapp/src/components/SongList.js
@@ -1,9 +1,9 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import uuid from "uuid/v1";
 import NewSongForm from "./NewSongForm";
 
 const SongList = () => {
-    //songs tabkic obiktów, setSong dodanie obiektu'piosenki' do dablicy
+  //songs tabkic obiktów, setSong dodanie obiektu'piosenki' do dablicy
   const [songs, setSongs] = useState([
     { title: "love song", id: 1 },
     { title: "nice song", id: 2 },
@@ -11,14 +11,21 @@ const SongList = () => {
   ]);
 
   //title prarametr króry przekazujemy w propsie
-  const addSong = (title) => {
+  const addSong = title => {
     setSongs([...songs, { title: title, id: uuid() }]);
   };
 
+  const [age, setAge] = useState(20);
+
   //useEfect jest uruchamiany za każdym razem przy renderowaniu komponentu , za kazdym razem zmiany danych w komponecie
-  useEffect(()=>{
-    console.log('useEffect hook ran', songs)
-  })
+  useEffect(() => {
+    console.log("useEffect hook ran", songs);
+  }, [songs]);
+  //dodanie drugiegi parametru [songs] ogranicza renderowanie do dnego elementu
+
+  useEffect(() => {
+    console.log("useEffect hook ran", age);
+  }, [age]);
   return (
     <div className="spng-list">
       <ul>
@@ -26,7 +33,8 @@ const SongList = () => {
           <li key={song.id}>{song.title}</li>
         ))}
       </ul>
-      <NewSongForm addSong={addSong}/>
+      <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
   );
 };
