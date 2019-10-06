a398b39cfb4466d64d098596b92a371881101d1a
MrozKarol <polityk357@gmail.com>
2019-10-06 19:07:24 +0200

    change the class component to function component

--- Contex_Hooks/contextapp/src/components/BookList.js
+++ Contex_Hooks/contextapp/src/components/BookList.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        console.log(this.context);
-        return (
-            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-                <ul>
-                    <li style={{ background: theme.ui }}>Witcher</li>
-                    <li style={{ background: theme.ui }}>Lord of the Rings</li>
-                    <li style={{ background: theme.ui }}>Game of Trones</li>
-                </ul>
-            </div>
-        );
-    }
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return (
+        <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+            <ul>
+                <li style={{ background: theme.ui }}>Witcher</li>
+                <li style={{ background: theme.ui }}>Lord of the Rings</li>
+                <li style={{ background: theme.ui }}>Game of Trones</li>
+            </ul>
+        </div>
+    );
 }
 
 export default BookList;
+
