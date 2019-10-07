06c50edc57d8b03c93abfd1de59a8872bbc6f940
MrozKarol <polityk357@gmail.com>
2019-10-07 19:49:20 +0200

    converted ThemeToggle class component to function component

--- Contex_Hooks/contextapp/src/components/ThemeToggle.js
+++ Contex_Hooks/contextapp/src/components/ThemeToggle.js
@@ -1,16 +1,10 @@
-import React,{Component} from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-class ThemeToggle extends Component {
-    static contextType = ThemeContext
-    render() { 
-        const {toggleTheme} = this.context;
-        
-        return ( 
-           
-            <button onClick={toggleTheme} >Toggle the theme</button>
-         );
-    }
-}
- 
-export default ThemeToggle;
+const ThemmeToggle = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+
+  return <button onClick={toggleTheme}>Toggle the theme</button>;
+};
+
+export default ThemmeToggle;
