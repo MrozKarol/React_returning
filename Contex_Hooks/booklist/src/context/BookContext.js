0232e5d0d0a4ea81bd09ecbbaa7fca27c8b10a9c
MrozKarol <polityk357@gmail.com>
2019-10-13 18:00:17 +0200

    instaled uuid, added BookContext

---
+++ Contex_Hooks/booklist/src/context/BookContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react';
+import uuid from 'uuid/v1'
+
+export const BookContext = createContext()
+
+const BookContextProvider = (props) => {
+    const [books, setBooks] = useState([
+        { title: 'witcher wild hund', author: 'andrzej sabkowski', id: 1 },
+        { title: 'game od thrones', author: 'gorge r.r. martin', id: 1 }
+    ])
+    const addBook = (title,author) =>{
+        setBooks([...books,{title:title},{author:author}, {id:uuid()} ])
+    } 
+    const removeBook = (id) =>{
+        setBooks(books.filter(book => book.id !==id));
+    }
+    return ( 
+        <BookContext.Provider value={{book,addBook,removeBook}}>
+            {props.children}
+        </BookContext.Provider>
+     );
+}
+
+export default BookContextProvider;
