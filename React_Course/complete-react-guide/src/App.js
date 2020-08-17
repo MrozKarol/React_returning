09eb964a369838b8b47ba4bb9f55023908c463cb
MrozKarol <polityk357@gmail.com>
2020-08-17 18:24:13 +0200

    Outputting Dynamic Content

--- React_Course/complete-react-guide/src/App.js
+++ React_Course/complete-react-guide/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi</h1>
         <Person/>
+        <Person/>
+        <Person/>
       </div>
      );
   }
--- React_Course/complete-react-guide/src/Person/Peron.js
+++ React_Course/complete-react-guide/src/Person/Peron.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const person = () => {
-    return <p>Iam a Perosn</p>
+return <p>I'am a Perosn and Iam {Math.floor(Math.random()*30)}</p>
 };
 
 export default person;
