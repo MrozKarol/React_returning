d9ac641f23f65cee6777231701036a054f6f4bab
MrozKarol <polityk357@gmail.com>
2020-08-18 18:07:13 +0200

    changed state by using function switchNameHandler

--- React_Course/complete-react-guide/src/App.js
+++ React_Course/complete-react-guide/src/App.js
@@ -1,20 +1,40 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Peron'
 
 class App extends Component {
-  render() { 
-    return ( 
+
+  state = {
+    persons: [
+      { name: 'Janek', age: 30 },
+      { name: 'Marek', age: 20 },
+      { name: 'Malina', age: 33 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('work')
+    this.setState({
+      persons: [
+        { name: 'Janusz', age: 30 },
+        { name: 'Marek', age: 20 },
+        { name: 'Malina', age: 22 },
+      ]
+    })
+  }
+  render() {
+    return (
       <div className="App">
         <h1>Hi</h1>
-        <Person name='Ada' age='20'/>
-        <Person name='Stefan' age='24'>My Hobbies: React</Person>
-        <Person name='Janek' age='20'/>
-  
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}  />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: React</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={this.switchNameHandler}>Swith Name</button>
+
       </div>
-     );
+    );
   }
 }
- 
+
 export default App;
 
