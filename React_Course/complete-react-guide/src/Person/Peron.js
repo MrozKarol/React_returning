9e5e656268e9dd4b99cb2714e0b5ceede3f97ce9
MrozKarol <polityk357@gmail.com>
2020-08-18 17:39:19 +0200

    use props children

--- React_Course/complete-react-guide/src/Person/Peron.js
+++ React_Course/complete-react-guide/src/Person/Peron.js
@@ -1,6 +1,11 @@
 import React from 'react';
 const person = (props) => {
-return <p>I'am a {props.name} and Iam {props.age} years old !</p>
+    return (
+        <div>
+            <p>I'am a {props.name} and Iam {props.age} years old !</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
