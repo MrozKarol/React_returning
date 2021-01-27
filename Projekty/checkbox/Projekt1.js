ebbdfa8cb86a114ebb8efbb0d23124f0719b358d
MrozKarol <polityk357@gmail.com>
2021-01-27 19:26:16 +0100

    format document

--- Projekty/checkbox/Projekt1.js
+++ Projekty/checkbox/Projekt1.js
@@ -2,49 +2,40 @@ const PositiveMessage = () => <p>Możesz obejrzeć film zaoraszamy</p>;
 
 const NegativeMessage = () => <p>Nie mozesz obejrześć filmu</p>;
 
-
-
-  
-
 class CheckboxAgeConfirmation extends React.Component {
+  state = {
+    isConfirmed: false,
+  };
 
-    state = {
-        isSonfirmed: false,
+  handleCheckboxChange = () => {
+    this.setState({
+      isConfirmed: !this.state.isConfirmed,
+    });
+  };
+
+  dispalyMesage = () => {
+    if (this.state.isConfirmed) {
+      return <PositiveMessage />;
+    } else if (!this.state.isConfirmed) {
+      return <NegativeMessage />;
     }
-
-    
-    
-    handleCheckboxChange = () => {
-        this.setState({
-            isSonfirmed: !this.state.isSonfirmed
-        })
-    }
-
-    dispalyMesage = () => {
-        if(this.state.isSonfirmed){
-            return <PositiveMessage/>
-        }else if (!this.state.isSonfirmed){
-            return <NegativeMessage/>
-        }
-        
-        
-    }
-    render(){
-        console.log(this.state.isSonfirmed)
-        return ( 
-            <>
-                <h1>Kup mi bilet na DeadPool</h1>
-                <input type="checkbox"  id="age" onChange={this.handleCheckboxChange} checked= {this.state.isSonfirmed} />
-                <label htmlFor="age">Mam co najmniej 16 lat</label>
-                {this.dispalyMesage()}
-            </>
-        )
-    }
-
-
-
-
-
+  };
+  render() {
+    console.log(this.state.isConfirmed);
+    return (
+      <>
+        <h1>Kup mi bilet na DeadPool</h1>
+        <input
+          type="checkbox"
+          id="age"
+          onChange={this.handleCheckboxChange}
+          checked={this.state.isConfirmed}
+        />
+        <label htmlFor="age">Mam co najmniej 16 lat</label>
+        {this.dispalyMesage()}
+      </>
+    );
+  }
 }
 
-ReactDOM.render(<CheckboxAgeConfirmation/>, document.getElementById('root'))
+ReactDOM.render(<CheckboxAgeConfirmation />, document.getElementById("root"));
