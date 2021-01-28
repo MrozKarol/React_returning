8f714aad52d0479dc8461233bba921c1ca8c5938
MrozKarol <polityk357@gmail.com>
2021-01-28 18:53:15 +0100

    buy a ticket, added checking if the box is selected

--- Projekty/checkbox/Projekt1.js
+++ Projekty/checkbox/Projekt1.js
@@ -1,38 +1,56 @@
-const PositiveMessage = () => <p>Możesz obejrzeć film zaoraszamy</p>;
+const PositiveMessage = () => <p>Możesz obejrzeć film zapraszamy</p>;
 
 const NegativeMessage = () => <p>Nie mozesz obejrześć filmu</p>;
 
 class CheckboxAgeConfirmation extends React.Component {
   state = {
     isConfirmed: false,
+    isFormSubmited: false,
   };
 
   handleCheckboxChange = () => {
     this.setState({
       isConfirmed: !this.state.isConfirmed,
+      isFormSubmited: false,
     });
   };
 
   dispalyMesage = () => {
-    if (this.state.isConfirmed) {
-      return <PositiveMessage />;
-    } else if (!this.state.isConfirmed) {
-      return <NegativeMessage />;
+    if (this.state.isFormSubmited) {
+      if (this.state.isConfirmed) {
+        return <PositiveMessage />;
+      } else if (!this.state.isConfirmed) {
+        return <NegativeMessage />;
+      }
+    }
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    // console.log("click")
+    if (!this.state.isFormSubmited) {
+      this.setState({
+        isFormSubmited: true,
+      });
     }
   };
   render() {
-    console.log(this.state.isConfirmed);
+    // console.log(this.state.isConfirmed);
     return (
       <>
         <h1>Kup mi bilet na DeadPool</h1>
-        <input
-          type="checkbox"
-          id="age"
-          onChange={this.handleCheckboxChange}
-          checked={this.state.isConfirmed}
-        />
-        <label htmlFor="age">Mam co najmniej 16 lat</label>
-        {this.dispalyMesage()}
+        <form onSubmit={this.handleFormSubmit}>
+          <input
+            type="checkbox"
+            id="age"
+            onChange={this.handleCheckboxChange}
+            checked={this.state.isConfirmed}
+          />
+          <label htmlFor="age">Mam co najmniej 16 lat</label>
+          {this.dispalyMesage()}
+          <br />
+          <button>Kup bilet</button>
+        </form>
       </>
     );
   }
