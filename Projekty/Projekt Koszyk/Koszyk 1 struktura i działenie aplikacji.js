daf82bec69af42ceabdb671bf7945de31e890b2b
MrozKarol <polityk357@gmail.com>
2021-02-03 18:39:46 +0100

    transfer of the css style file to be prepared from the cart project

--- Projekty/Projekt Koszyk/Koszyk 1 struktura i działenie aplikacji.js
+++ Projekty/Projekt Koszyk/Koszyk 1 struktura i działenie aplikacji.js
@@ -1,23 +1,36 @@
 // Projekt koszyk struktura i działanie aplikacji
 class App extends React.Component {
-    state = {
-        availableProducts: 6,
-        shoppingCart: 0,
-    }
-    handleRemoveFromCard = () => (
-        this.setState({shoppingCart: this.state.shoppingCart -1})
-    )
-    handleAddFromCard = () =>(
-        this.setState({shoppingCart: this.state.shoppingCart +1})
-    )
-    render(){
-        return(
-            <div>
-                <button disabled = {this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCard} >-</button>
-                <span> {this.state.shoppingCart} </span>
-                <button disabled= {this.state.shoppingCart === this.state.availableProducts ? true : false} onClick={this.handleAddFromCard} > +</button>
-            </div>
-        )
-    }
+  state = {
+    availableProducts: 6,
+    shoppingCart: 0,
+  };
+  handleRemoveFromCard = () =>
+    this.setState({ shoppingCart: this.state.shoppingCart - 1 });
+  handleAddFromCard = () =>
+    this.setState({ shoppingCart: this.state.shoppingCart + 1 });
+  render() {
+    return (
+      <div>
+        <button
+          disabled={this.state.shoppingCart ? false : true}
+          onClick={this.handleRemoveFromCard}
+        >
+          -
+        </button>
+        <span> {this.state.shoppingCart} </span>
+        <button
+          disabled={
+            this.state.shoppingCart === this.state.availableProducts
+              ? true
+              : false
+          }
+          onClick={this.handleAddFromCard}
+        >
+          {" "}
+          +
+        </button>
+      </div>
+    );
+  }
 }
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
