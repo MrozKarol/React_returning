432ecfeb25fd91cbb41e5f692a250172b622b7e3
MrozKarol <polityk357@gmail.com>
2022-01-19 17:05:01 +0100

    add search first and last name

--- Projekty/list-payment/src/App.js
+++ Projekty/list-payment/src/App.js
@@ -6,14 +6,14 @@ import UserList from './UserList';
 
 class App extends Component {
   state = {
-      select : 'name',
-      firstName: '',
-      lastName : '',
-      department : '',
-      salary : 0,
-      currency: 'usd',
-      searchFirstName: '',
-      searchLastName: '',
+    select: 'name',
+    firstName: '',
+    lastName: '',
+    department: '',
+    salary: 0,
+    currency: 'usd',
+    searchFirstName: '',
+    searchLastName: '',
     users: [
       {
         id: 1,
@@ -28,7 +28,7 @@ class App extends Component {
         firstName: "Jane",
         lastName: "Doe",
         department: "IT",
-        salary: 3000.5 ,
+        salary: 3000.5,
         currency: "usd"
       },
       {
@@ -66,34 +66,36 @@ class App extends Component {
   }
 
 
-  handleChange = (e)=> {
+  handleChange = (e) => {
     // console.log(e.target.type)
     // console.log(e.target.name)
     const name = e.target.name
     const value = e.target.value
     this.setState({
-      [name] : value,
-      
+      [name]: value,
+
     })
-    
+
   }
 
-  handleSubmit = (e) =>{
+  handleSubmit = (e) => {
     e.preventDefault()
     const users = [...this.state.users]
-    const name  = this.state.firstName
+    const name = this.state.firstName
     const lastName = this.state.lastName
     const department = this.state.department
     const salary = this.state.salary
     const currency = this.state.currency
-    let id = users.length+1
+    let id = users.length + 1
     console.log(id)
-    users.push({id:id,firstName: name, lastName: lastName,department:department,salary:salary, currency:currency})
+    users.push({ id: id, firstName: name, lastName: lastName, department: department, salary: salary, currency: currency })
     this.setState({
-      users}
+      users
+    }
     )
     this.setState({
-      id:id,firstName: name, lastName: lastName,department:department,salary: 0, currency:'usd'}
+      id: id, firstName: name, lastName: lastName, department: department, salary: 0, currency: 'usd'
+    }
     )
     // this.setState({
     //   usersSearch : users
@@ -101,61 +103,89 @@ class App extends Component {
 
   }
 
-  handleSubmitSearch = (e) =>{
+  handleSubmitSearch = (e) => {
     e.preventDefault()
     const searchFirstName = this.state.searchFirstName
     const searchLastName = this.state.searchLastName
     const usersSearch = [...this.state.usersSearch]
     const users = [...this.state.users]
-      if(searchLastName||searchFirstName){
-        const nUsers =  users.filter((user)=>user.firstName.toLowerCase() === `${searchFirstName}`.toLowerCase())
-  
-        const lUsers =  users.filter((user)=>user.lastName.toLowerCase() === `${searchLastName}`.toLowerCase())
-        // console.log(nUsers)
-        // console.log(lUsers)
-        const allUsersSearch = nUsers.concat(lUsers)
-        
-        function filterDuplicate(myArr, prop) {
-       
-          var res = {};
-          var resArr = [];
-          for (var elem of myArr) {
-            res[elem.id] = elem;
-          }
-          for (let [index, elem] of Object.entries(res)) {
-            resArr.push(elem);
-          }
-          return resArr;
-      }
-      
-    let finalRes = filterDuplicate(allUsersSearch,"id");
-    console.log("finalResults : ",finalRes);
-      
-        this.setState({
-          usersSearch : finalRes
-        })      
-      }    
-   }
+    if (searchFirstName) {
+      const nUsers = users.filter((user) => user.firstName.toLowerCase() === `${searchFirstName}`.toLowerCase())
+   
+      function getUnique(arr, comp) {
 
-   handleUsersFilter = (e) =>{
+        return arr
+          .map(e => e[comp])
+          .map((e, i, final) => final.indexOf(e) === i && i)
+          .filter(e => arr[e]).map(e => arr[e]);
+      }
+
+      let finalRes = getUnique(nUsers, 'id');
+      console.log("finalResults : ", finalRes);
+
+      this.setState({
+        usersSearch: finalRes
+      })
+    }
+    if (searchLastName) {
+      const lUsers = users.filter((user) => user.lastName.toLowerCase() === `${searchLastName}`.toLowerCase())
+   
+      function getUnique(arr, comp) {
+
+        return arr
+          .map(e => e[comp])
+          .map((e, i, final) => final.indexOf(e) === i && i)
+          .filter(e => arr[e]).map(e => arr[e]);
+      }
+
+      let finalRes = getUnique(lUsers, 'id');
+      console.log("finalResults : ", finalRes);
+
+      this.setState({
+        usersSearch: finalRes
+      })
+    }
+    if (searchLastName && searchFirstName) {
+      const allUsers = users.filter((user) => user.lastName.toLowerCase() === `${searchLastName}`.toLowerCase() && user.firstName.toLowerCase() === `${searchFirstName}`.toLowerCase())
+      function getUnique(arr, comp) {
+
+        return arr
+          .map(e => e[comp])
+          .map((e, i, final) => final.indexOf(e) === i && i)
+          .filter(e => arr[e]).map(e => arr[e]);
+      }
+
+      let finalRes = getUnique(allUsers, 'id');
+      console.log("finalResults : ", finalRes);
+
+      this.setState({
+        usersSearch: finalRes
+      })
+    }
     
-   }
+    
+  }
+  
+
+  handleUsersFilter = (e) => {
+
+  }
   render() {
     return (
       <div className="App">
-      
-      <form onSubmit={this.handleSubmit}>
+
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="user">Name:
             <input type="text" id='firstName' name="firstName" value={this.state.firstName} onChange={this.handleChange} />
           </label>
           <label htmlFor="user">Last Name:
-            <input type="text" id='lastName'  name="lastName" value={this.state.lastName} onChange={this.handleChange} />
+            <input type="text" id='lastName' name="lastName" value={this.state.lastName} onChange={this.handleChange} />
           </label>
           <label htmlFor="user">Departament:
-            <input type="text" id='department'  name="department" value={this.state.department} onChange={this.handleChange} />
+            <input type="text" id='department' name="department" value={this.state.department} onChange={this.handleChange} />
           </label>
           <label htmlFor="user">Salary:
-            <input type="number" step="0.01" min="0" id='salary'  name="salary" value={this.state.salary} onChange={this.handleChange} />
+            <input type="number" step="0.01" min="0" id='salary' name="salary" value={this.state.salary} onChange={this.handleChange} />
           </label>
           <label>
             <select id="currency" name="currency" value={this.state.currency} onChange={this.handleChange}>
@@ -167,20 +197,20 @@ class App extends Component {
           <UserList users={this.state.users}></UserList>
         </form>
         <div>
-        <form  onSubmit={this.handleSubmitSearch}>
+          <form onSubmit={this.handleSubmitSearch}>
             <label htmlFor="user">Name:
-              <input type="text" id='searchFirstName' name="searchFirstName" value={this.state.searchFirstName} onChange={this.handleChange} onClick={this.handleUsersFilter}  />
+              <input type="text" id='searchFirstName' name="searchFirstName" value={this.state.searchFirstName} onChange={this.handleChange} onClick={this.handleUsersFilter} />
             </label>
             <label htmlFor="user">Last Name:
-              <input type="text" id='searchLastName' name="searchLastName" value={this.state.searchLastName} onChange={this.handleChange}   />
+              <input type="text" id='searchLastName' name="searchLastName" value={this.state.searchLastName} onChange={this.handleChange} />
             </label>
             <button >Szukaj</button>
             <UserList users={this.state.usersSearch}></UserList>
           </form>
-          
-     
-        </div> 
-      
+
+
+        </div>
+
 
       </div>
     );
